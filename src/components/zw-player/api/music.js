1f8ab37cf0a045cba60cd764b5c451e84a824351
Haitao <jianght945@gmail.com>
2023-06-21 16:04:08 +1000

    edit music api

--- src/components/zw-player/api/music.js
+++ src/components/zw-player/api/music.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 //获取歌词
 export const getWords = id => {
-  return axios.post(`https://cloud-music-api-wayne.vercel.app/lyric?id=${id}`);
+  return axios.post(`https://static.jianght.eu.org/music/lyric?id=${id}`);
 };
 //获取歌曲详情
 export const getMusicInfo = id => {
-  return axios.post(`https://cloud-music-api-wayne.vercel.app/song/detail?ids=${id}`);
+  return axios.post(`https://static.jianght.eu.org/music/song/detail?ids=${id}`);
 };
 //获取歌曲url
 export const getMusicUrl = id => {
-  return axios.post(`https://cloud-music-api-wayne.vercel.app/song/url?id=${id}`);
+  return axios.post(`https://static.jianght.eu.org/music/song/url?id=${id}`);
 };
 //获取热门歌曲
 export const getHotMusic = id => {
-  return axios.post(`https://cloud-music-api-wayne.vercel.app/playlist/detail?id=${id}`);
+  return axios.post(`https://static.jianght.eu.org/music/playlist/detail?id=${id}`);
 };
 //获取搜索建议
 export const getSearchSuggest = key => {
-  return axios.post(`https://cloud-music-api-wayne.vercel.app/search/suggest?keywords=${key}`);
+  return axios.post(`https://static.jianght.eu.org/music/search/suggest?keywords=${key}`);
 };
 //获取歌曲热门评论
 export const getHotTalk = id => {
-  return axios.post(`https://cloud-music-api-wayne.vercel.app/comment/music?id=${id}&limit=3`);
+  return axios.post(`https://static.jianght.eu.org/music/comment/music?id=${id}&limit=3`);
 };
